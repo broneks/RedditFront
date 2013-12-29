fc16e4ffb9732cbba0340da089caaf1080a92280
Bszulc <bronekszulc@gmail.com>
2013-12-29 18:18:04 -0500

    clean up

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import flask.views
 import json
-import urllib
+import urllib2
 import time
 
 app = flask.Flask(__name__)
 
 class Main(flask.views.MethodView):
     def get(self):
-        page = urllib.urlopen('http://www.reddit.com/.json')
+        # creating custom user agent
+        req = urllib2.Request('http://www.reddit.com/.json', None, { 'User-agent': """Mozilla/5.0 (Windows NT 6.1; WOW64) 
+                                                                    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.
+                                                                    1650.63 Safari/537.36 | by bviddy""" })
+        page = urllib2.urlopen(req)
         j = json.loads(page.read())
-        images = []
         
+        images = []
+        # pulling image urls from the json object
         for c in j['data']['children']:
             if (c['data']['url'][-3:] in ('jpg', 'png', 'gif')) or (c['data']['url'][-4:] == 'jpeg'):
                 images.append(c['data']['url'])
             elif c['data']['url'][7:12] == 'imgur':
                 images.append(c['data']['url'] + '.jpg')
-                
+        
+        # setting current date and time    
         d = time.strftime("%a, %B %d, %Y")
         t = time.strftime("%H:%M:%S")
         
