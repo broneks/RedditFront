d8f40d950eb50240634b6aa36ca4e069e4083ad6
Bszulc <bronekszulc@gmail.com>
2013-12-29 22:08:15 -0500

    adding links to original posts

--- app.py
+++ app.py
@@ -8,25 +8,26 @@ app = flask.Flask(__name__)
 class Main(flask.views.MethodView):
     def get(self):
         # creating custom user agent
+        ROOT_URL = 'http://www.reddit.com/'
         req = urllib2.Request('http://www.reddit.com/.json', None, { 'User-agent': """Mozilla/5.0 (Windows NT 6.1; WOW64) 
                                                                     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.
-                                                                    1650.63 Safari/537.36 | by Bszulc""" })
+                                                                    1650.63 Safari/537.36 by Bszulc""" })
         page = urllib2.urlopen(req)
         j = json.loads(page.read())
         
         images = []
-        # pulling image urls from the json object
+        # pulling image urls and post links from json object
         for c in j['data']['children']:
-            if (c['data']['url'][-3:] in ('jpg', 'png', 'gif')) or (c['data']['url'][-4:] == 'jpeg'):
-                images.append(c['data']['url'])
+            if (c['data']['url'][-3:] in ('jpg', 'png', 'gif')) or c['data']['url'][-4:] == 'jpeg':
+                images.append([c['data']['url'], ROOT_URL + c['data']['permalink']])
             elif c['data']['url'][7:12] == 'imgur':
-                images.append(c['data']['url'] + '.jpg')
+                images.append([c['data']['url'] + '.jpg', ROOT_URL + c['data']['permalink']])
         
         # setting current date and time    
         d = time.strftime("%a, %B %d, %Y")
         t = time.strftime("%H:%M:%S")
         
-        return flask.render_template('base.html', images=images, date=d, time=t)
+        return flask.render_template('base.html', date=d, time=t, images=images)
 
 
 app.add_url_rule('/', view_func=Main.as_view('main'), methods=['GET'])
